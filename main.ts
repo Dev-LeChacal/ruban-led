2a16fe63c653ea114ceef529b5310d1a0a1e144a
LeChacal <dev.lechacal@gmail.com>
2025-09-27 11:05:34 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,44 @@
-input.onButtonPressed(Button.A, function () {
-    let actual_led = 0
+input.onButtonPressed(Button.AB, function () {
+    if (is_countdown) {
+        let actual_led = 0
 
-    for (let a = 3; a >= 1; a--) {
+        for (let a = 3; a >= 1; a--) {
 
-        draw_number(a)
+            draw_number(a)
 
-        for (let b = 0; b <= 9; b++) {
-            set_leds_color(0, actual_led, NeoPixelColors.Black)
-            set_leds_color(actual_led, strip.length(), get_color_with_number(a))
+            for (let b = 0; b <= 9; b++) {
+                set_leds_color(0, actual_led, NeoPixelColors.Black)
+                set_leds_color(actual_led, strip.length(), get_color_for_number(a))
 
-            actual_led += 1
+                actual_led += 1
 
-            pause(100)
+                pause(100)
+            }
         }
-    }
 
-    draw_number(0)
-    set_leds_color(0, strip.length(), NeoPixelColors.Green)
+        draw_number(0)
+        set_leds_color(0, strip.length(), NeoPixelColors.Green)
+
+        pause(250)
+
+        set_leds_color(0, strip.length(), NeoPixelColors.Black)
+        draw_number(-1)
+
+        is_countdown = false
+    }
 })
 
+input.onButtonPressed(Button.A, function () {
+    if (!is_countdown) {
+        a_points += 1
+    }
+})
+
+input.onButtonPressed(Button.B, function() {
+    if (!is_countdown) {
+        b_points += 1
+    }
+})
 
 function set_leds_color(start: number, end: number, color: NeoPixelColors) {
     for (let i = start; i <= end; i++) {
@@ -32,7 +52,7 @@ function set_led_color(color: NeoPixelColors, pixelOffset: number) {
     strip.setPixelColor(pixelOffset, neopixel.colors(color))
 }
 
-function get_color_with_number(num: number) {
+function get_color_for_number(num: number) {
     let color = NeoPixelColors.Black
 
     if (num == 3) {
@@ -47,7 +67,8 @@ function get_color_with_number(num: number) {
 }
 
 function draw_number(num: number) {
-    basic.clearScreen()
+    clear_screen()
+
     if (num == 3) {
         led.plot(1, 0)
         led.plot(2, 0)
@@ -95,4 +116,38 @@ function draw_number(num: number) {
     }
 }
 
+function clear_screen() {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            led.unplot(x, y)
+        }
+    }
+}
+
+function draw_leds_for_player(points: number, color: NeoPixelColors) {
+    const strip_Length = strip.length();
+    const laps = Math.floor(points / strip_Length);
+
+    const start = points - (strip_Length * laps)
+    const end = (points + player_length) - (strip_Length * laps)
+
+    set_leds_color(start, end, color)
+}
+
+basic.forever(function() {
+    if (!is_countdown) {
+        set_leds_color(0, strip.length(), NeoPixelColors.Black)
+
+        draw_leds_for_player(a_points, NeoPixelColors.Red)
+        draw_leds_for_player(b_points, NeoPixelColors.Blue)
+    }
+})
+
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+
+let is_countdown = true
+
+let a_points = 0
+let b_points = 0
+
+const player_length = 2
