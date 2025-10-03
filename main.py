64adbf46b566d8ef1b32542485587c1b162ab00b
LeChacal <dev.lechacal@gmail.com>
2025-10-03 15:41:38 +0200

    Mise à jour main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,120 @@
+def on_button_pressed_ab():
+    global is_countdown
+    if is_countdown:
+        actual_led = 0
+        a = 3
+        while a >= 1:
+            draw_number(a)
+            for b in range(10):
+                set_leds_color(0, actual_led, NeoPixelColors.BLACK)
+                set_leds_color(actual_led, len(strip), get_color_for_number(a))
+                actual_led += 1
+                pause(100)
+            a -= 1
+        draw_number(0)
+        set_leds_color(0, len(strip), NeoPixelColors.GREEN)
+        pause(250)
+        set_leds_color(0, len(strip), NeoPixelColors.BLACK)
+        draw_number(-1)
+        is_countdown = False
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 def on_button_pressed_a():
-    change_led_color(5, NeoPixelColors.ORANGE)
-    
+    global is_countdown, a_points
+    if not is_countdown:
+        a_points += 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def change_led_color(color: NeoPixelColors, position: number):
-    strip.set_pixel_color(position, color)
+def on_button_pressed_b():
+    global is_countdown, b_points
+    if not is_countdown:
+        b_points += 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def set_leds_color(start: number, end: number, color: NeoPixelColors):
+    i = start
+    while i <= end:
+        set_led_color(color, i)
+        i += 1
+    strip.show()
+def set_led_color(color2: NeoPixelColors, pixelOffset: number):
+    strip.set_pixel_color(pixelOffset, neopixel.colors(color2))
+def get_color_for_number(num: number):
+    color3 = NeoPixelColors.BLACK
+    if num == 3:
+        color3 = NeoPixelColors.RED
+    elif num == 2:
+        color3 = NeoPixelColors.ORANGE
+    elif num == 1:
+        color3 = NeoPixelColors.YELLOW
+    return color3
+def draw_number(num2: number):
+    clear_screen()
+    if num2 == 3:
+        led.plot(1, 0)
+        led.plot(2, 0)
+        led.plot(3, 0)
+        led.plot(3, 1)
+        led.plot(2, 2)
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+    elif num2 == 2:
+        led.plot(1, 0)
+        led.plot(2, 0)
+        led.plot(3, 0)
+        led.plot(3, 1)
+        led.plot(2, 2)
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+    elif num2 == 1:
+        led.plot(2, 0)
+        led.plot(1, 1)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+    elif num2 == 0:
+        led.plot(1, 0)
+        led.plot(2, 0)
+        led.plot(3, 0)
+        led.plot(1, 1)
+        led.plot(3, 1)
+        led.plot(1, 2)
+        led.plot(3, 2)
+        led.plot(1, 3)
+        led.plot(3, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+def clear_screen():
+    for x in range(5):
+        for y in range(5):
+            led.unplot(x, y)
+def draw_leds_for_player(points: number, color4: NeoPixelColors):
+    strip_Length = len(strip)
+    laps = Math.floor(points / strip_Length)
+    start2 = points - (strip_Length * laps)
+    end2 = (points + player_length) - (strip_Length * laps)
+    set_leds_color(start2, end2, color4)
+
+def on_forever():
+    global is_countdown
+    if not is_countdown:
+        set_leds_color(0, len(strip), NeoPixelColors.BLACK)
+        draw_leds_for_player(a_points, NeoPixelColors.RED)
+        draw_leds_for_player(b_points, NeoPixelColors.BLUE)
+basic.forever(on_forever)
 
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+is_countdown = True
+a_points = 0
+b_points = 0
+player_length = 2
--- main.ts
+++ main.ts
@@ -1,75 +1,72 @@
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let actual_led: number;
+    let a: number;
+    
     if (is_countdown) {
-        let actual_led = 0
-
-        for (let a = 3; a >= 1; a--) {
-
+        actual_led = 0
+        a = 3
+        while (a >= 1) {
             draw_number(a)
-
-            for (let b = 0; b <= 9; b++) {
+            for (let b = 0; b < 10; b++) {
                 set_leds_color(0, actual_led, NeoPixelColors.Black)
                 set_leds_color(actual_led, strip.length(), get_color_for_number(a))
-
                 actual_led += 1
-
                 pause(100)
             }
+            a -= 1
         }
-
         draw_number(0)
         set_leds_color(0, strip.length(), NeoPixelColors.Green)
-
         pause(250)
-
         set_leds_color(0, strip.length(), NeoPixelColors.Black)
         draw_number(-1)
-
         is_countdown = false
     }
+    
 })
-
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     if (!is_countdown) {
         a_points += 1
     }
+    
 })
-
-input.onButtonPressed(Button.B, function() {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     if (!is_countdown) {
         b_points += 1
     }
+    
 })
-
-function set_leds_color(start: number, end: number, color: NeoPixelColors) {
-    for (let i = start; i <= end; i++) {
+function set_leds_color(start: number, end: number, color: number) {
+    let i = start
+    while (i <= end) {
         set_led_color(color, i)
+        i += 1
     }
-
     strip.show()
 }
 
-function set_led_color(color: NeoPixelColors, pixelOffset: number) {
-    strip.setPixelColor(pixelOffset, neopixel.colors(color))
+function set_led_color(color2: number, pixelOffset: number) {
+    strip.setPixelColor(pixelOffset, neopixel.colors(color2))
 }
 
-function get_color_for_number(num: number) {
-    let color = NeoPixelColors.Black
-
+function get_color_for_number(num: number): number {
+    let color3 = NeoPixelColors.Black
     if (num == 3) {
-        color = NeoPixelColors.Red
+        color3 = NeoPixelColors.Red
     } else if (num == 2) {
-        color = NeoPixelColors.Orange
+        color3 = NeoPixelColors.Orange
     } else if (num == 1) {
-        color = NeoPixelColors.Yellow
+        color3 = NeoPixelColors.Yellow
     }
-
-    return color
+    
+    return color3
 }
 
-function draw_number(num: number) {
+function draw_number(num2: number) {
     clear_screen()
-
-    if (num == 3) {
+    if (num2 == 3) {
         led.plot(1, 0)
         led.plot(2, 0)
         led.plot(3, 0)
@@ -80,7 +77,7 @@ function draw_number(num: number) {
         led.plot(1, 4)
         led.plot(2, 4)
         led.plot(3, 4)
-    } else if (num == 2) {
+    } else if (num2 == 2) {
         led.plot(1, 0)
         led.plot(2, 0)
         led.plot(3, 0)
@@ -91,7 +88,7 @@ function draw_number(num: number) {
         led.plot(1, 4)
         led.plot(2, 4)
         led.plot(3, 4)
-    } else if (num == 1) {
+    } else if (num2 == 1) {
         led.plot(2, 0)
         led.plot(1, 1)
         led.plot(2, 1)
@@ -100,7 +97,7 @@ function draw_number(num: number) {
         led.plot(1, 4)
         led.plot(2, 4)
         led.plot(3, 4)
-    } else if (num == 0) {
+    } else if (num2 == 0) {
         led.plot(1, 0)
         led.plot(2, 0)
         led.plot(3, 0)
@@ -114,40 +111,36 @@ function draw_number(num: number) {
         led.plot(2, 4)
         led.plot(3, 4)
     }
+    
 }
 
 function clear_screen() {
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x < 5; x++) {
+        for (let y = 0; y < 5; y++) {
             led.unplot(x, y)
         }
     }
 }
 
-function draw_leds_for_player(points: number, color: NeoPixelColors) {
-    const strip_Length = strip.length();
-    const laps = Math.floor(points / strip_Length);
-
-    const start = points - (strip_Length * laps)
-    const end = (points + player_length) - (strip_Length * laps)
-
-    set_leds_color(start, end, color)
+function draw_leds_for_player(points: number, color4: number) {
+    let strip_Length = strip.length()
+    let laps = Math.floor(points / strip_Length)
+    let start2 = points - strip_Length * laps
+    let end2 = points + player_length - strip_Length * laps
+    set_leds_color(start2, end2, color4)
 }
 
-basic.forever(function() {
+basic.forever(function on_forever() {
+    
     if (!is_countdown) {
         set_leds_color(0, strip.length(), NeoPixelColors.Black)
-
         draw_leds_for_player(a_points, NeoPixelColors.Red)
         draw_leds_for_player(b_points, NeoPixelColors.Blue)
     }
+    
 })
-
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
-
 let is_countdown = true
-
 let a_points = 0
 let b_points = 0
-
-const player_length = 2
+let player_length = 2
